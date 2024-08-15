4f54679ffe28e92facb90701bb2862a07fe33836
Kauan <kauanmiyaguisecco@gmail.com>
2024-08-15 14:12:46 -0300

    Add files via upload

---
+++ bem vindo.py
@@ -0,0 +1,14 @@
+nome=input('Qual o seu nome?:')
+print('Olá', nome, ', esse é seu nome?')
+
+nome=input('Qual é o seu nome?')
+idade=input('Qual é sua idade?')
+peso=input('Qual é seu peso?')
+print(nome, idade, peso)
+
+n1 = int(input('Primeiro número:'))
+n2 = int(input('Segundo número:'))
+s = n1 + n2
+print('Se juntar {0} com {1}, dá {2}'.format(n1, n2, s))
+if{(n1.isalpha):
+       (print('DIGITA UM NÚMERO'))}:
